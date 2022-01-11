a1ce66420dc01e14507362d50f64f27dd208b8d7
alex <javafirst17@gmail.com>
2022-01-11 21:44:49 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,13 @@ import java.io.IOException;
 import java.util.function.UnaryOperator;
 
 public class Main {
+    public static final double LEARNING_RATE = 0.001;
+    public static final UnaryOperator<Double> SIGMOID = x -> 1 / (1 + Math.exp(-x));
+    public static final UnaryOperator<Double> D_SIGMOID = y -> y * (1 - y);
+    public static final String TRAIN_PATH = "./dataset/digits/train";
+    public static final int EPOCHS = 1000;
+    public static final int BATCH_SIZE = 100;
+
 
     public static void main(String[] args) {
         dots();
@@ -17,14 +24,12 @@ public class Main {
     }
 
     private static void digits() throws IOException {
-        UnaryOperator<Double> sigmoid = x -> 1 / (1 + Math.exp(-x));
-        UnaryOperator<Double> dsigmoid = y -> y * (1 - y);
-        NeuralNetwork nn = new NeuralNetwork(0.001, sigmoid, dsigmoid, 784, 512, 128, 32, 10);
+        NeuralNetwork nn = new NeuralNetwork(LEARNING_RATE, SIGMOID, D_SIGMOID, 784, 512, 128, 32, 10);
 
         int samples = 60000;
         BufferedImage[] images = new BufferedImage[samples];
         int[] digits = new int[samples];
-        File[] imagesFiles = new File("./dataset/digits/train").listFiles();
+        File[] imagesFiles = new File(TRAIN_PATH).listFiles();
         for (int i = 0; i < samples; i++) {
             images[i] = ImageIO.read(imagesFiles[i]);
             digits[i] = Integer.parseInt(imagesFiles[i].getName().charAt(10) + "");
@@ -39,11 +44,11 @@ public class Main {
             }
         }
 
-        int epochs = 1000;
+        int epochs = EPOCHS;
         for (int i = 1; i < epochs; i++) {
             int right = 0;
             double errorSum = 0;
-            int batchSize = 100;
+            int batchSize = BATCH_SIZE;
             for (int j = 0; j < batchSize; j++) {
                 int imgIndex = (int)(Math.random() * samples);
                 double[] targets = new double[10];
